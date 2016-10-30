8596fb21af7fa89df865721a6ee268ca6b7910d6
Andrew <andrewfvitale@gmail.com>
2016-10-29 20:52:29 -0400

    fixes to the MecanumDriveSystem

--- TeamCode/src/main/java/net/ftc/tdt2845/RedAutonomous.java
+++ TeamCode/src/main/java/net/ftc/tdt2845/RedAutonomous.java
@@ -6,6 +6,7 @@ package net.ftc.tdt2845;
 
 
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -16,7 +17,7 @@ import net.ftc.tdt2845.robot.subsystems.MecanumDriveSystem;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-@TeleOp(name="RedAutonomous", group="Linear Opmode")
+@Autonomous(name="RedAutonomous", group="Linear Opmode")
 public class RedAutonomous extends LinearOpMode
 {
 
@@ -30,22 +31,37 @@ public class RedAutonomous extends LinearOpMode
 
     @Override
     public void runOpMode() throws InterruptedException {
+        telemetry.addData("message", "before mecanum");
+        telemetry.update();
 
         MecanumDriveSystem mecanumDriveSystem = new MecanumDriveSystem (this);
         //declare the variable for TDTColorSensor
         TDTColorSensor colorSensor = null;
 
-
+        telemetry.addData("message", "before calibrate");
+        telemetry.update();
         mecanumDriveSystem.calibrate();
+        telemetry.addData("message", "after calibrate");
+        telemetry.update();
         waitForStart();
+        telemetry.addData("message", "after waitForStart");
+        telemetry.update();
         runtime.reset();
+        telemetry.addData("message", "after reset");
+        telemetry.update();
+        telemetry.addData("message", "after update");
+        telemetry.update();
 
         //Strafing by Power, distance in inches, and direction(right or left)
 //        autonomousDriveSystem.strafe(1, 10, right);
 
         //Moves forward by power and distance in inches
         mecanumDriveSystem.goForward(12, .5);
+        telemetry.addData("message", "after goForward");
+        telemetry.update();
         mecanumDriveSystem.turnRight(90);
+        telemetry.addData("message", "after turnRight");
+        telemetry.update();
 
         //Color sensor code thata ctivates once the color is ditected, //TODO Test code
 //        colorSensor = new TDTColorSensor(hardwareMap, telemetry);
